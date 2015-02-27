b87284b0f3deaa7c4baaf817b90d2186a63623df
Alex <alexlaraalex@gmail.com>
2015-02-26 21:21:11 -0800

    fix sequelize and migration bugs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express"),
   bodyParser = require("body-parser"),
   app = express();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
